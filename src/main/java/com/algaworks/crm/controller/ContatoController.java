5b5c25fc999273bd235734c5982ca6787c37c9d8
m19u31 <m19u31@outlook.com>
2023-05-30 16:18:23 -0300

    put method

--- src/main/java/com/algaworks/crm/controller/ContatoController.java
+++ src/main/java/com/algaworks/crm/controller/ContatoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,6 +40,19 @@ public class ContatoController {
 	public String deletar(@RequestBody Contato contato) {
 		contatoRepository.delete(contato);
 		return "contato deletado";
+	}
+	
+	@PutMapping
+	public String atualizar(@RequestBody Contato contato) {
+		if (contato.getId() != null) {
+			contatoRepository.save(contato);
+			return "contato salvo";
+		}
+		else {
+			
+			return "contato não encontrado";
+		}
+		
 	}
 	
 
